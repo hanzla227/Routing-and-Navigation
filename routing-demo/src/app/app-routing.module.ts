134f773246722dedce2b4705dfe38c232c3df305
hanzla227 <m.hanzla7854@gmail.com>
2021-06-28 18:18:00 +0500

    Route Parameters

--- routing-demo/src/app/app-routing.module.ts
+++ routing-demo/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListItIsComponent } from './employee-list-it-is/employee-list-it-is.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  
+  //{path:'' , component:EmployeeListItIsComponent},
+  {path:'' , redirectTo:'/departments',pathMatch:'full'},
   {path :'departments', component:DepartmentListComponent},
-  {path:'employees', component:EmployeeListItIsComponent}
+  {path:'employees', component:EmployeeListItIsComponent},
+  {path:"**", component:PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -13,4 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents=[DepartmentListComponent, EmployeeListItIsComponent];
+export const routingComponents=[DepartmentListComponent, EmployeeListItIsComponent , PageNotFoundComponent];
---
+++ routing-demo/src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `
+    <h3 style="text-align:center;" >page not found</h3>
+  `,
+  styles: [
+  ]
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
